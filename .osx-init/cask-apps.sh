ccd93e3d9bb2669f4af34460810b11472f5852c8
Camil Blanaru <camil@edka.io>
2016-01-07 00:34:25 +0100

    first commit

---
+++ .osx-init/cask-apps.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+brew cask install \
+ 1password \
+ knox \
+ infinit \
+ atom \
+ dropbox \
+ transmit \
+ google-chrome \
+ flux \
+ iterm2 \
+ mplayerx \
+ unetbootin \
+ little-snitch \
+ sourcetree \
+ github-desktop \
+ gpgtools \
+ dockertoolbox \
+ vmware-fusion \
+ kaleidoscope \
+ keepingyouawake \
+ paw \
+ screenhero \
+ sequel-pro \
+ slack \
+ spectacle \
+ sublime-text3 \
+ virtualbox \
+ viscosity \
+ vlc
